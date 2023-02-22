5edee37c25541a216a873b30de687a4ed29f3eec
McDonald <104572589+McDonaldMusimwa@users.noreply.github.com>
2023-02-22 14:59:24 +0200

    changed the product rendering of the site
    Added productDetails and moved logic to the his file

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import {  getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
-const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+
+const dataSource = new ProductData('tents');
+const productId = getParam('product');
+console.log(dataSource.findProductById(productId))
+
+const product = new ProductDetails(productId,dataSource);
+product.init();
 // add to cart button event handler
+/*
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
@@ -14,5 +19,6 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
+  .getElementById('addToCart')
   .addEventListener("click", addToCartHandler);
+*/
